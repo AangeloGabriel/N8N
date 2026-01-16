498bbc8cc9f9c5e8ea22a2549d065dd5495596d3
AangeloGabriel <alvesangelo402@gmail.com>
2026-01-16 13:33:07 -0300

    Atualizações: Decidi juntar o processador com o consolidador e finalizei o processador.

---
+++ processador.py
@@ -0,0 +1,19 @@
+from organizador import OrdenaLista
+import pandas as pd
+import os
+
+def Processa():
+
+    ListaOrdenada = OrdenaLista()
+    Tabela = pd.DataFrame()
+
+    for data, lista_caminhos in ListaOrdenada.items():
+            for caminhos in lista_caminhos:
+                if data == os.path.basename(caminhos):
+                    excel = pd.read_excel(caminhos)
+                    excel.insert(0, "DataAnuncio", value=(os.path.splitext(data)[0]).replace("_", "/"), allow_duplicates=True)
+                    Tabela = pd.concat([Tabela, excel], ignore_index=True)
+                else:
+                     continue
+    return Tabela
+
